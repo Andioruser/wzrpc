fb3f127c017e2485cb5f6187c90c30918c623afa
wenzhuo jiang <3544346335@qq.com>
2025-01-20 14:17:01 +0800

    init net-address

---
+++ rpc/net/net_address.h
@@ -0,0 +1,62 @@
+#ifndef RPC_NET_NET_ADDRESS_H
+#define RPC_NET_NET_ADDRESS_H
+
+#include <memory>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+namespace rpc {
+
+class NetAddress {
+
+public:
+
+    typedef std::shared_ptr<NetAddress> ptr;
+    
+    virtual sockaddr* getSocketAddr() = 0;
+
+    virtual int getFamily() const = 0;
+
+    virtual std::string toString() const = 0;
+
+    virtual socklen_t getSockLen() const = 0;
+
+};
+
+class IPAddress : public NetAddress {
+
+public:
+    IPAddress(const std::string&ip, uint16_t port);
+
+    IPAddress(const std::string& addr);
+
+    IPAddress(u_int16_t port);
+
+    IPAddress(sockaddr* addr);
+
+    sockaddr* getSocketAddr();
+
+    int getFamily() const;
+
+    std::string toString() const;
+
+    socklen_t getSockLen() const;
+
+    std::string get_ip() const {return m_ip;}
+
+    uint16_t get_port() const {return m_port;}
+
+public:
+    static bool checkIvalidIPAddr(const std::string& ip);
+    
+private:
+
+    std::string m_ip;
+    uint16_t m_port;
+    sockaddr_in m_addr;
+};
+
+
+}
+#endif
+
